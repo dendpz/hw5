01cf6d7e288871025d2c17112ce4ee33a01361d2
Dennis <depazdennis@gmail.com>
2024-03-07 04:44:59 -0800

    final2

--- js/ex2.js
+++ js/ex2.js
@@ -1,6 +1,6 @@
 const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const oddNums = numberList.filter(odd => odd % 3 === 0);
+const oddNums = numberList.filter(odd => odd % 2 !== 0);
 const div2or5 = numberList.filter(num => num % 2 === 0 || num % 5 === 0);
 const divSquare = numberList.filter(num => num % 3 === 0).map(x => x  ** 2);
 const sumDivSquare =  numberList.filter(num => num % 5 === 0).map(x => x  ** 2).reduce((a, b) => a + b, 0);
